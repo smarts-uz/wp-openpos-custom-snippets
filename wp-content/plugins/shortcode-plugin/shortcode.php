c06ee6be42282dfaa28fe1f9f8f27cc894370f25
alpha20060309 <komoliddin487@gmail.com>
2025-03-10 14:29:16 +0500

    Add functionality to Shortcode plugin. Give links in page to posts with db operation.

--- wp-content/plugins/shortcode-plugin/shortcode.php
+++ wp-content/plugins/shortcode-plugin/shortcode.php
@@ -33,3 +33,66 @@ function sp_handle_student_data($attributes){
 
 // Shortcode with DB operation
 
+add_shortcode("list-posts", "sp_handle_list_posts_wp_query_class");
+
+function sp_handle_list_posts(){
+
+    global $wpdb;
+
+    $table_prefix = $wpdb->prefix; //wp_
+    $table_name = $table_prefix . "posts"; // wp_posts
+
+
+    //Get post whose post_type = post and post_status = publish
+
+    $posts = $wpdb->get_results(
+        "SELECT post_title from {$table_name} WHERE post_type = 'post' AND post_status = 'publish'"
+    );
+
+    if(count($posts) > 0){
+
+        $outputHTML = "<ul>";
+
+
+        foreach($posts as $post){
+            $outputHTML .= '<li>'.$post->post_title.'</li>';
+        }
+
+        $outputHTML .= "</ul>";
+
+        return $outputHTML;
+    }
+
+
+    return 'No Post Found';
+}
+
+// [list-posts number="10"]
+
+function sp_handle_list_posts_wp_query_class($attributes){
+    $attributes = shortcode_atts(array(
+        "number" => 6
+    ), $attributes, "list-posts");
+
+    $query = new WP_Query(array(
+        "posts_per_page" => $attributes['number'],
+        "post_status" => "publish"
+    ));
+
+    if($query-> have_posts()){
+
+        $outputHTML = '<ul>';
+
+        while($query->have_posts()){
+            $query->the_post();
+            $outputHTML .= '<li class="my_class"><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>'; // Hello World
+            // $outputHTML .= '<li class="my_class">'.get_the_title().'</li>';
+        }
+
+        $outputHTML .= '</ul>';
+
+        return $outputHTML;
+    }
+
+    return "No post found";
+}
